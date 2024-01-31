a9e8677be4d1fc31d989c556ca15c4e0e2427629
서상상 <115199442+dayeon-seo@users.noreply.github.com>
2024-01-31 12:06:20 +0900

    __init__.py 완료

--- transformer/__init__.py
+++ transformer/__init__.py
@@ -1,3 +1,4 @@
+# transformer 패키지를 이용하기 위하여 가져온다.
 import transformer.Constants
 import transformer.Modules
 import transformer.Layers
@@ -6,6 +7,7 @@ import transformer.Models
 import transformer.Translator
 import transformer.Optim
 
+# transformer 패키지에서 필요한 값들을 이용하기 위하여 꺼내는 것이다.
 __all__ = [
     transformer.Constants, transformer.Modules, transformer.Layers,
     transformer.SubLayers, transformer.Models, transformer.Optim,
