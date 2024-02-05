a198d99cd8e9e101e26b1afb6d78a096f085d8e2
서상상 <115199442+dayeon-seo@users.noreply.github.com>
2024-02-05 22:18:11 +0900

    Update Modules.py

--- transformer/Modules.py
+++ transformer/Modules.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F # PyTorch의 함수 계산을 할 수 있게 해
 __author__ = "Yu-Hsiang Huang" # 저자~
 
 class ScaledDotProductAttention(nn.Module): # Scaled Dot-ProductAttention을 구현한다. 쿼리, 키, 값을 이용하여 가중치를 계산한다.
-    # 어텐션 스코어는 쿼리와 키 사이의 관계를 나타내는 값이다.
+    # 어텐션 스코어는 쿼리와 키 사이의 유사도를 나타내는 값이다.
     ''' Scaled Dot-Product Attention ''' 
 
     def __init__(self, temperature, attn_dropout=0.1):
