8d775302bf1944942f13a119deadfc589eaffc4f
서상상 <115199442+dayeon-seo@users.noreply.github.com>
2024-02-05 18:48:21 +0900

    Update Models.py 완료

--- transformer/Models.py
+++ transformer/Models.py
@@ -85,7 +85,7 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    ''' A decoder model with self attention mechanism. '''
+    ''' A decoder model with self attention mechanism. ''' # self-attention 메커니즘을 갖는 디코더 모델이다.
 
     def __init__(
             self, n_trg_vocab, d_word_vec, n_layers, n_head, d_k, d_v,
@@ -93,32 +93,44 @@ class Decoder(nn.Module):
 
         super().__init__()
 
-        self.trg_word_emb = nn.Embedding(n_trg_vocab, d_word_vec, padding_idx=pad_idx)
-        self.position_enc = PositionalEncoding(d_word_vec, n_position=n_position)
-        self.dropout = nn.Dropout(p=dropout)
-        self.layer_stack = nn.ModuleList([
-            DecoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)
+        self.trg_word_emb = nn.Embedding(n_trg_vocab, d_word_vec, padding_idx=pad_idx) # nn.Embedding은 n_trg_vocab과 d_word_vec에 따라서 단어를 벡터로 매핑해주는 파라미터화된 층이다.
+                # 텍스트 데이터의 단어를 연속적인 벡터 공간으로 매핑하는 것에 사용된다.
+                # n_trg_vocab은 목효하는 언어의 어휘의 크기다. 모델이 학습할 수 있는 고유한 단어의 수다.
+                # d_word_vec은 단어를 임베딩하는 데 사용되는 벡터의 차원이다. (임베딩은 문장 또는 개체를 고차원 벡터 공간에 매핑하는 기법이다.)
+                # padding_idx는 패딩을 위한 인덱스다.
+        self.position_enc = PositionalEncoding(d_word_vec, n_position=n_position) # 위치 인코딩을 해준다. (위에 PositionalEncoding 나옴.)
+        self.dropout = nn.Dropout(p=dropout) # 드롭아웃을 적용한다.
+        self.layer_stack = nn.ModuleList([ # nn.ModuleList를 사용해서 디코더 레이어를 쌓을 수 있다.
+            DecoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout) # DecoderLayer라는 클래스를 정의하고, 클래스의 인스턴트들을 nn.ModuleList에 담아서 쌓아 놓는 것이다.
+            # d_model은 디코더 레이어의 입력 및 출력 차원의 수다. d_inner는 디코더 레이어 내부의 FeedForward 신경망에서 중간에 있는 차원의 수다.
             for _ in range(n_layers)])
-        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
-        self.scale_emb = scale_emb
-        self.d_model = d_model
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6) # 레이어 정규화를 수행한다.
+        self.scale_emb = scale_emb # 임베딩 스케일 조절 여부다.
+        self.d_model = d_model # 모델의 임베딩 차원이다.
 
-    def forward(self, trg_seq, trg_mask, enc_output, src_mask, return_attns=False):
+    def forward(self, trg_seq, trg_mask, enc_output, src_mask, return_attns=False): # 디코더의 순전파 연산을 정의한다.
+        # trg_seqp는 목표 언어 시퀀스다. 디코더의 입력으로 사용된다.
+        # trg_mask는 목표 언어 시퀀스에서 패딩을 마스킹한 텐서다.
+        # enc_output은 인코더의 출력이다.
+        # return_attns는 어텐션 결과를 반환할지 여부를 결정한다.
 
-        dec_slf_attn_list, dec_enc_attn_list = [], []
+        dec_slf_attn_list, dec_enc_attn_list = [], [] # 어텐션 메커니즘의 결과를 저장하기 위한 빈 리스트다.
 
         # -- Forward
-        dec_output = self.trg_word_emb(trg_seq)
-        if self.scale_emb:
+        dec_output = self.trg_word_emb(trg_seq) # 목표 언어의 시퀀스를 단어 임베딩으로 매핑한다.
+        # nn.Embedding 층을 사용해서 단어를 임베딩 벡터로 바꾼다.
+        if self.scale_emb: # scale_emb가 true면 임베딩 출력에 self.d_model에 아래의 연산을 해서 임베딩을 스케일링한다.
             dec_output *= self.d_model ** 0.5
-        dec_output = self.dropout(self.position_enc(dec_output))
-        dec_output = self.layer_norm(dec_output)
+        dec_output = self.dropout(self.position_enc(dec_output)) # 임베딩에 드롭아웃을 적용한다.
+        dec_output = self.layer_norm(dec_output) # 레이어 정규화를 수행한다.
 
-        for dec_layer in self.layer_stack:
-            dec_output, dec_slf_attn, dec_enc_attn = dec_layer(
+        for dec_layer in self.layer_stack: # 디코더의 레이어 스택을 반복한다.
+            # 반복문을 동태서 디코더의 각 레이어에 입력을 전달하고 각 레이어에서의 어텐션 결과를 기록할 수 있다.
+            # 디코더의 순전파가 이루어지게 된다.
+            dec_output, dec_slf_attn, dec_enc_attn = dec_layer( # 각 디코더 레이어에 dec_output과 enc_output을 전달해서 디코더 레이어의 순전파 연산을 수행한다.
                 dec_output, enc_output, slf_attn_mask=trg_mask, dec_enc_attn_mask=src_mask)
-            dec_slf_attn_list += [dec_slf_attn] if return_attns else []
-            dec_enc_attn_list += [dec_enc_attn] if return_attns else []
+            dec_slf_attn_list += [dec_slf_attn] if return_attns else [] # return_attns가 True면 self-attention 결과인 dec_slf_attn을 어텐션 리스트인 dec_slf_attn_list에 추가한다.
+            dec_enc_attn_list += [dec_enc_attn] if return_attns else [] # return_attns가 True면 enc-dec 결과인 dec_enc_attn을 어텐션 리스트인 dec_enc_attn_list에 추가한다.
 
         if return_attns:
             return dec_output, dec_slf_attn_list, dec_enc_attn_list
